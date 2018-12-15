c321b06c629853764c3958e1dbd4fe4e39f631a5
Mutasim Billah Bin Ahmad <fueanta@gmail.com>
2018-12-15 18:01:27 +0600

    LoginFormCookieAdded

--- USP_Application/Controllers/HomeController.cs
+++ USP_Application/Controllers/HomeController.cs
@@ -12,9 +12,11 @@ namespace USP_Application.Controllers
 {
     public class HomeController : Controller
     {
+        IUserLogin userLoginRepository;
         ICity cityRepository;
-        public HomeController(ICity ctRepo)
+        public HomeController(IUserLogin ulRepo, ICity ctRepo)
         {
+            userLoginRepository = ulRepo;
             cityRepository = ctRepo;
         }
         public ActionResult Index()
@@ -39,6 +41,78 @@ namespace USP_Application.Controllers
         public ActionResult Register()
         {
             return RedirectToAction("Insert", "Client");
+        }
+
+        public ActionResult Login()
+        {
+            var viewModel = new LoginFormViewModel();
+            viewModel.UserLogin = new UserLogin();
+
+            if (Request.Cookies["LoginEmail"] != null)
+            {
+                // populate data from cookie
+                viewModel.UserLogin.Email = Request.Cookies["LoginEmail"].Value;
+                viewModel.UserLogin.Password = Request.Cookies["LoginPassword"].Value;
+                viewModel.RememberMe = true;
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult LogInSubmit(LoginFormViewModel viewModel)
+        {
+            var user = userLoginRepository.GetAll().FirstOrDefault(u => u.Email.Equals(viewModel.UserLogin.Email) && u.Password.Equals(viewModel.UserLogin.Password));
+
+            if (user != null)
+            {
+                if (viewModel.RememberMe)
+                {
+                    // store cookie for 1 day
+                    HttpCookie loginEmailCookie = new HttpCookie("LoginEmail");
+                    loginEmailCookie.Expires = DateTime.Now.AddDays(1d);
+                    loginEmailCookie.Value = user.Email;
+                    Response.Cookies.Add(loginEmailCookie);
+
+                    HttpCookie loginPasswordCookie = new HttpCookie("LoginPassword");
+                    loginPasswordCookie.Expires = DateTime.Now.AddDays(1d);
+                    loginPasswordCookie.Value = user.Password;
+                    Response.Cookies.Add(loginPasswordCookie);
+                }
+                else
+                {
+                    // delete cookie
+                    if (Request.Cookies["LoginEmail"] != null)
+                    {
+                        HttpCookie loginEmailCookie = Request.Cookies["LoginEmail"];
+                        loginEmailCookie.Expires = DateTime.Now.AddDays(-1d);
+                        Response.Cookies.Add(loginEmailCookie);
+
+                        HttpCookie loginPasswordCookie = Request.Cookies["LoginPassword"];
+                        loginPasswordCookie.Expires = DateTime.Now.AddDays(-1d);
+                        Response.Cookies.Add(loginPasswordCookie);
+                    }
+                }
+
+                switch (user.UserType)
+                {
+                    case UserType.Client:
+                        // logged in user is client
+                        return RedirectToAction("Index", "Client");
+                    case UserType.Employee:
+                        // logged in user is employee
+                        return RedirectToAction("Index", "Employee");
+                    case UserType.Labour:
+                        // logged in user is labour
+                        return RedirectToAction("Index", "Labour");
+                    default:
+                        // logged in user is admin
+                        return RedirectToAction("Index", "Admin");
+                }
+            }
+
+            viewModel.Invalid = true;
+            return View("Login", viewModel);
         }
     }
 }
--- USP_Application/Models/LabourUserLoginModel.cs
+++ USP_Application/Models/LabourUserLoginModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace USP_Application.Models
+﻿namespace USP_Application.Models
 {
     public class LabourUserLoginModel
     {
